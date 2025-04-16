9a00a3bbf5230433c6b562d5aea56bee16c90aeb
DanGonzaG <dan.g.g@hotmail.es>
2025-04-15 18:37:14 -0600

    Se crea proyecto

---
+++ G4_CasoEstudio2/G4_CasoEstudio2.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace G4_CasoEstudio2.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
